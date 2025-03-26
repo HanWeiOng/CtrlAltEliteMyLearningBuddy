e06a1bf8b9646fecabde31515dea57c013792e2c
peiyilee <peiyi.lee.2022@smu.edu.sg>
2025-03-27 00:42:40 +0800

    updated on retrival of question both frontend and backend

--- backend/db/question.sql
+++ backend/db/question.sql
@@ -1,6 +1,9 @@
 CREATE TABLE IF NOT EXISTS question (
     id SERIAL PRIMARY KEY,
     paper_name VARCHAR(255),
+    subject VARCHAR(50),
+    banding VARCHAR(50),
+    level VARCHAR(50),
     page_number INT,
     question_number INT,
     question_text TEXT,
