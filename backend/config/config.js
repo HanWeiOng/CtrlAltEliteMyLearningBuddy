566ebe685e50730ae19526d4858bc97f84936cb6
peiyilee <peiyi.lee.2022@smu.edu.sg>
2025-04-15 19:23:05 +0800

    removed credentials

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 
 module.exports = {
   development: {
-    username: "postgres", // Use your RDS PostgreSQL username
-    password: "ctrlaltelite", // Use your RDS PostgreSQL password
-    database: "exam_papers_test", // Use your RDS PostgreSQL database name
-    host: "database-1.cv6m2giwwgvo.ap-southeast-2.rds.amazonaws.com", // Replace with your RDS endpoint
+    username:  // Use your RDS PostgreSQL username
+    password:  // Use your RDS PostgreSQL password
+    database:  // Use your RDS PostgreSQL database name
+    host: // Replace with your RDS endpoint
     port: 5432, // Default PostgreSQL port
     dialect: "postgres", // Specifies the database dialect
   },
